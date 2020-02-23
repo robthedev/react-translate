e94a37d1414b515097d7462d5b9a118c8fd53c7e
robthewebdev <robthewebdev@gmail.com>
2020-02-22 23:12:54 -0600

    Updates

---
+++ precache-manifest.cfa5d8d05458daaabcd2e8110a96a3f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca6a45a3d16aff67acc064ffd9208a7",
+    "url": "/reacti18n/index.html"
+  },
+  {
+    "revision": "18d2e4500de6b25f49d6",
+    "url": "/reacti18n/static/css/main.4b099052.chunk.css"
+  },
+  {
+    "revision": "a48ec0c594ab02acd57a",
+    "url": "/reacti18n/static/js/2.cd750850.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reacti18n/static/js/2.cd750850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d2e4500de6b25f49d6",
+    "url": "/reacti18n/static/js/main.53ca3006.chunk.js"
+  },
+  {
+    "revision": "5afec5e8599749d99f6b",
+    "url": "/reacti18n/static/js/runtime-main.ac71d5c8.js"
+  },
+  {
+    "revision": "38f88714d1f6b481d01651439dfed026",
+    "url": "/reacti18n/static/media/323310.38f88714.svg"
+  },
+  {
+    "revision": "7b6d1881becf7096857fdd8feeb3de54",
+    "url": "/reacti18n/static/media/323365.7b6d1881.svg"
+  }
+]);
